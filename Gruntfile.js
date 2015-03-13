e0ae36451cdbee09a95ad9886d6b2b4612b95f40
Brock Beldham <bbeldham@trapeze.com>
2015-03-13 11:42:25 -0400

    setup router and switching pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -124,7 +124,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-livereload');
 
     grunt.registerTask('default', [
         'jshint',
---
+++ static/js/initialize.js
@@ -0,0 +1,69 @@
+(function () {
+    var global = this;
+
+    var _        = global._;
+    var $        = global.jQuery;
+    var Backbone = global.Backbone;
+
+    var Portfolio = (global.Portfolio || (global.Portfolio = { }));
+
+    var Core = Portfolio.Core = function (options) {
+
+        var defaults = {
+            selectors: {
+                homePage: '#home',
+                workPage: '#work',
+                contactPage: '#contact'
+            }
+        };
+
+        this.config         = $.extend(true, defaults, options || { });
+
+        this._initialize();
+    };
+
+    Core.prototype._initialize = function () {
+        this._initRouter();
+    };
+
+    Core.prototype._initRouter = function () {
+        var that = this;
+        var MyRouter = Backbone.Router.extend({
+            routes: {
+                ""        : "home",
+                "work"    : "work",
+                "contact" : "contact"
+            },
+        
+            home: function() {
+                that._openPage('#home');
+            },
+        
+            work: function() {
+                that._openPage('#work');
+            },
+        
+            contact: function() {
+                that._openPage('#contact');
+            }
+        });
+
+        new MyRouter();
+        Backbone.history.start();
+    };
+
+    Core.prototype._openPage = function (pageId) {
+        $(".container").hide();
+        $(pageId).show();
+    };
+
+    // Auto instantiate
+    $(function () {
+        var $portolio = $('.portfolio');
+
+        _($portolio).each(function () {
+            new Core();
+        });
+    });
+
+}).call(this);
