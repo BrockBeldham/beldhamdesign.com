a9d8ffa63de102fd6860a08dd58968c7dfa6dd1c
Brock Beldham <Brock.Beldham@gmail.com>
2015-04-06 23:18:59 -0400

    responsive styles for about page

--- includes/about.php
+++ includes/about.php
@@ -3,23 +3,33 @@
         <h2>About</h2>
         <div class="about-left">
             <img src="static/img/about/main.jpg">
-            <div class="icon-list">
-                <a href="mailto:brockbeldham@gmail.com" target="_blank" class="icon-link">
-                    <i class="fa fa-envelope"></i>
-                </a>
-                <a href="https://www.facebook.com/brock.beldham" target="_blank" class="icon-link">
-                    <i class="fa fa-facebook"></i>
-                </a>
-                <a href="https://twitter.com/BrockBeldham" target="_blank" class="icon-link">
-                    <i class="fa fa-twitter"></i>
-                </a>
-                <a href="https://github.com/BrockBeldham" target="_blank" class="icon-link">
-                    <i class="fa fa-github"></i>
-                </a>
-                <a href="https://www.linkedin.com/profile/view?id=98919640" target="_blank" class="icon-link">
-                    <i class="fa fa-linkedin"></i>
-                </a>
-            </div>
+            <ul class="icon-list">
+                <li class="icon-list-item">
+                    <a href="mailto:brockbeldham@gmail.com" target="_blank" class="icon-link">
+                        <i class="fa fa-envelope"></i>
+                    </a>
+                </li>
+                <li class="icon-list-item">
+                    <a href="https://www.facebook.com/brock.beldham" target="_blank" class="icon-link">
+                        <i class="fa fa-facebook"></i>
+                    </a>
+                </li>
+                <li class="icon-list-item">
+                    <a href="https://twitter.com/BrockBeldham" target="_blank" class="icon-link">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                </li>
+                <li class="icon-list-item">
+                    <a href="https://github.com/BrockBeldham" target="_blank" class="icon-link">
+                        <i class="fa fa-github"></i>
+                    </a>
+                </li>
+                <li class="icon-list-item">
+                    <a href="https://www.linkedin.com/profile/view?id=98919640" target="_blank" class="icon-link">
+                        <i class="fa fa-linkedin"></i>
+                    </a>
+                </li>
+            </ul>
             <p class="accolades">
                 Developer <strong>/</strong>
                 Designer <strong>/</strong>
