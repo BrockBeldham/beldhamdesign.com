6adb7faf48e481ae0a08c918c25adac7b542c36c
Brock Beldham <Brock.Beldham@gmail.com>
2015-05-03 21:38:02 -0400

    a little more proofreading and wrote site headline

--- includes/home.php
+++ includes/home.php
@@ -1,6 +1,6 @@
 <section id="home" class="container">
     <div class="home">
-        <h3>Lorem ipsum dolor sit amet <strong>consectetur</strong>, <strong>adipiscing</strong>, <strong>sed eiusmod</strong>. tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</h3>
+        <h3>Hi, my name is Brock. I like to make <strong>innovative</strong>, <strong>engaging</strong>, <strong>and awesome</strong> web applications. Check out my work and let's start something awesome.</h3>
         <div class="icon-list">
             <a href="mailto:brockbeldham@gmail.com" target="_blank" class="icon-link">
                 <i class="fa fa-envelope"></i>
