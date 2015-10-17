ebceb3b8ca6f3c6e12d9160f6361218bef31bbad
Brock Beldham <Brock.Beldham@gmail.com>
2015-10-17 13:12:11 -0400

    added resume to build

--- includes/home.php
+++ includes/home.php
@@ -1,47 +1,68 @@
 <section id="home" class="container">
     <div class="home">
         <h2>Hi, my name is Brock. I like to make <strong>innovative</strong>, <strong>engaging</strong>, <strong>and awesome</strong> web applications. Check out my work and let's start something awesome.</h2>
-        <div class="icon-list">
-            <a href="mailto:brockbeldham@gmail.com"
-            target="_blank"
-            class="icon-link"
-            data-event-category="home"
-            data-event-action="contacts"
-            data-event-label="email">
-                <i class="fa fa-envelope"></i>
-            </a>
-            <a href="https://www.facebook.com/brock.beldham"
-            target="_blank"
-            class="icon-link"
-            data-event-category="home"
-            data-event-action="contacts"
-            data-event-label="facebook">
-                <i class="fa fa-facebook"></i>
-            </a>
-            <a href="https://twitter.com/BrockBeldham"
-            target="_blank"
-            class="icon-link"
-            data-event-category="home"
-            data-event-action="contacts"
-            data-event-label="twitter">
-                <i class="fa fa-twitter"></i>
-            </a>
-            <a href="https://github.com/BrockBeldham"
-            target="_blank"
-            class="icon-link"
-            data-event-category="home"
-            data-event-action="contacts"
-            data-event-label="github">
-                <i class="fa fa-github"></i>
-            </a>
-            <a href="https://www.linkedin.com/profile/view?id=98919640"
-            target="_blank"
-            class="icon-link"
-            data-event-category="home"
-            data-event-action="contacts"
-            data-event-label="linkedin">
-                <i class="fa fa-linkedin"></i>
-            </a>
+        <ul class="icon-list">
+            <li class="icon-list-item">
+                <a href="mailto:brockbeldham@gmail.com"
+                target="_blank"
+                class="icon-link"
+                data-event-category="home"
+                data-event-action="contacts"
+                data-event-label="email">
+                    <i class="fa fa-envelope"></i>
+                </a>
+            </li>
+            <li class="icon-list-item">
+                <a href="https://www.facebook.com/brock.beldham"
+                target="_blank"
+                class="icon-link"
+                data-event-category="home"
+                data-event-action="contacts"
+                data-event-label="facebook">
+                    <i class="fa fa-facebook"></i>
+                </a>
+            </li>
+            <li class="icon-list-item">
+                <a href="https://twitter.com/BrockBeldham"
+                target="_blank"
+                class="icon-link"
+                data-event-category="home"
+                data-event-action="contacts"
+                data-event-label="twitter">
+                    <i class="fa fa-twitter"></i>
+                </a>
+            </li>
+            <li class="icon-list-item">
+                <a href="https://github.com/BrockBeldham"
+                target="_blank"
+                class="icon-link"
+                data-event-category="home"
+                data-event-action="contacts"
+                data-event-label="github">
+                    <i class="fa fa-github"></i>
+                </a>
+            </li>
+            <li class="icon-list-item">
+                <a href="https://www.linkedin.com/profile/view?id=98919640"
+                target="_blank"
+                class="icon-link"
+                data-event-category="home"
+                data-event-action="contacts"
+                data-event-label="linkedin">
+                    <i class="fa fa-linkedin"></i>
+                </a>
+            </li>
+            <li class="icon-list-item">
+                <a href="static/docs/resume.pdf"
+                target="_blank"
+                download
+                class="icon-link"
+                data-event-category="home"
+                data-event-action="contacts"
+                data-event-label="resume">
+                    <i class="fa fa-file-text"></i>
+                </a>
+            </li>
         </div>
     </div>
 </section>
